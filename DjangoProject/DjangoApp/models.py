c4949e80cf1f7c01c0ad7e241503a749c3ec5043
Mr.RySes <mpoprev2@email.com>
2023-06-22 05:51:22 +0800

    06-22-23 05:51

--- DjangoProject/DjangoApp/models.py
+++ DjangoProject/DjangoApp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class users(models.Model):
+	id = models.CharField(max_length=11, primary_key=True)
 	username = models.CharField(max_length=25)
 	email = models.CharField(max_length=50, default="")
 	password = models.CharField(max_length=100)
